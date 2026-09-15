ed5fdb169289a2c459f020c3e529c606a424de44
Lucas LeMeilleur <lucas.pokrywa@gmail.com>
2026-09-15 15:16:15 +0200

    correction

--- install.bat
+++ install.bat
@@ -1,8 +1,7 @@
 python -m venv .venv
 
-.venv\Scripts\activate.bat
+call .venv\Scripts\activate.bat
 
-.venv\Scripts\pip.exe pip install -r requirements.txt
+pip install -r requirements.txt
 
-
-.venv\Scripts\python.exe src\main.py
+python src\main.py
